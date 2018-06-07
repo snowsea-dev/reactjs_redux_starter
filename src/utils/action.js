8885268534a1d33487a5c2d234bfcb5130e4e2ff
SnowSea <snowsea.dream@gmail.com>
2018-06-07 23:21:32 +0400

    add redux action utils

---
+++ src/utils/action.js
@@ -0,0 +1,21 @@
+// utils for redux actions and creators
+export const request = (type) => `${type}/request`
+export const success = (type) => `${type}/success`
+export const failure = (type) => `${type}/failure`
+
+export const requestCreator = (type, payload) => ({
+  type: request(type),
+  payload,
+})
+
+export const successCreator = (type, payload) => ({
+  type: success(type),
+  payload,
+})
+
+export const failureCreator = (type, payload) => ({
+  type: failure(type),
+  payload,
+})
+
+export const originalType = (type) => (type.substr(0, type.length - 8)) // 8 is length of '/request', '/success', '/failure'
